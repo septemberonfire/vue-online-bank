419ee3981b0289c0b74d392283815d96c0e39422
deadbirdscream <89587459+deadbirdscream@users.noreply.github.com>
2023-10-04 18:44:19 +0300

    added import order with trivago package for prettier

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from '@/App.vue'
-import router from './router'
-import '@/theme.css'
 import 'ant-design-vue/dist/reset.css';
+import { createPinia } from 'pinia';
+import { createApp } from 'vue';
 
-const pinia = createPinia()
+import App from '@/App.vue';
+import router from '@/router';
+import '@/theme.css';
 
-createApp(App)
-.use(router)
-.use(pinia)
-.mount('#app')
+const pinia = createPinia();
+
+createApp(App).use(router).use(pinia).mount('#app');
